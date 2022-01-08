7929489f6fd895cf1df95bf220cbc74f1da97128
elicavalheiro <elimara.ac@gmail.com>
2022-01-08 03:08:29 -0300

    feat(datocms): implement DatoCMS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { useQuery } from 'graphql-hooks';
+
+const INTRO_QUERY = `query getIntro {
+  intro {
+    jobTitle
+    photo {
+      id
+      url
+      alt
+    }
+  }
+}`;
+
 function App(): JSX.Element {
-  return <div>Hello World</div>;
+  const { loading, error, data } = useQuery(INTRO_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <></>;
+
+  return (
+    <div>
+      {data && (
+        <div>
+          <img
+            src={data.intro.photo.url}
+            alt={data.intro.photo.alt}
+            style={{ width: '100px' }}
+          />
+          <p>{data?.intro?.jobTitle}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
+import { ClientContext, GraphQLClient } from 'graphql-hooks';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new GraphQLClient({
+  url: 'https://graphql.datocms.com/',
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_DATOCMS_API_KEY}`,
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ClientContext.Provider value={client}>
+      <App />
+    </ClientContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
