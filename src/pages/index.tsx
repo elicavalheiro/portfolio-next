604a7eec09b2fcda0a0cc24cb209821aafafa426
elicavalheiro <elimara.ac@gmail.com>
2022-01-08 23:01:08 -0300

    feat(all): add configs

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <title>Eli Cavalheiro</title>
+      <meta name="description" content="Front-end Developer" />
+    </Head>
+
+    <main>Hello World</main>
+  </div>
+);
+
+export default Home;
